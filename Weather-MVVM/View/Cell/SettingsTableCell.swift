a7eb77f4d9ab2ea794afa89b683e679796bc6a39
imran <imufun2@gmail.com>
2021-01-23 20:57:50 +0600

    comment delete code

--- Weather-MVVM/View/Cell/SettingsTableCell.swift
+++ Weather-MVVM/View/Cell/SettingsTableCell.swift
@@ -20,27 +20,19 @@ class SettingsTableCell: UITableViewCell {
     func setupUI(){
         
         let mainContainer = UIView()
- 
+        
         backgroundColor = .clear
         mainContainer.layer.cornerRadius = 5
-//        mainContainer.backgroundColor = hexToUIColor(hex:"#F7F5ED")
+        //        mainContainer.backgroundColor = hexToUIColor(hex:"#F7F5ED")
         contentView.addSubview(mainContainer)
         mainContainer.position(top: contentView.topAnchor, left: contentView.leadingAnchor, bottom: contentView.bottomAnchor, right: contentView.trailingAnchor, insets: UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0))
         
         
-        
-          cityLbl = MyUILabel(frame: .zero, text: "Rangpur", textColor: buttonColor, fontSize: UIFont.systemFont(ofSize: 18), textAlign: .left)
+        cityLbl = MyUILabel(frame: .zero, text: "Rangpur", textColor: buttonColor, fontSize: UIFont.systemFont(ofSize: 18), textAlign: .left)
         mainContainer.addSubview(cityLbl!)
         cityLbl!.position(top: mainContainer.topAnchor, left: mainContainer.leadingAnchor, insets: .init(top: 10, left: 20, bottom: 10, right: 0))
         cityLbl?.size(width:120)
         //        cityLbl.backgroundColor = .yellow
-        
-        
-//          degreeLbl = MyUILabel(frame: .zero, text: "12", textColor: buttonColor, fontSize: UIFont.systemFont(ofSize: 18), textAlign: .right)
-//        mainContainer.addSubview(degreeLbl!)
-//        degreeLbl?.position(top: mainContainer.topAnchor, right: mainContainer.trailingAnchor, insets: .init(top: 10, left: 0, bottom: 10, right: 20))
-//        degreeLbl?.size(width:120)
-        //        degreeLbl.backgroundColor = .yellow
         
         
     }
